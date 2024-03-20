0f8319d21d89ed6a9215d67ad4968ad76813b5f4
Alexander Romanov <alex.rom23@mail.ru>
2024-03-20 17:09:54 +0300

    home-manager

--- flake.nix
+++ flake.nix
@@ -1,22 +1,39 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  description = "NixOS configuration";
 
-    # helix editor, use the master branch
-    helix.url = "github:helix-editor/helix/master";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # home-manager, used for managing user configuration
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
-      modules = [
-        ./configuration.nix
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      my-nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
 
-        # This module works the same as the `specialArgs` parameter we used above
-        # chose one of the two methods to use
-        # { _module.args = { inherit inputs; };}
-      ];
+          # make home-manager as a module of nixos
+          # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.alexander = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+        ];
+      };
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,40 @@
+{config, pkgs, ...}:
+{
+  home.username = "alexander";
+  home.homeDirectory = "/home/alexander";
+  home.stateVersion = "23.11";  
+  home.packages = with pkgs; [
+    neofetch
+    ripgrep
+    eza
+    zip
+    tree
+    btop
+    git
+    vim
+    helix
+  ];
+  
+  programs.git = {
+    enable = true;
+    userName = "Alexander Romanov";
+    userEmail = "alex.rom23@mail.ru";
+  };
+
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+  };
+
+  programs.home-manager.enable = true;
+}
