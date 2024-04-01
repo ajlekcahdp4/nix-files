380ec3de2ee2b1c108af8ed41e340cc2d263202c
Alexander Romanov <alex.rom23@mail.ru>
2024-04-01 21:52:33 +0300

    neovim setup

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -10,6 +10,8 @@
 }: {
   # You can import other home-manager modules here
   imports = [
+    inputs.nixvim.homeManagerModules.nixvim
+    # For NixOS
     # If you want to use modules your own flake exports (from modules/home-manager):
     # outputs.homeManagerModules.example
 
@@ -19,6 +21,77 @@
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
   ];
+
+  programs.nixvim = {
+    enable = true;
+    enableMan = true;
+    colorschemes.catppuccin = {
+      enable = true;
+      flavour = "mocha";
+      showBufferEnd = true;
+      dimInactive = {
+        enabled = true;
+        percentage = 0.15;
+        shade = "dark";
+      };
+      integrations = {
+        cmp = true;
+        which_key = true;
+        telescope.enabled = true;
+      };
+      styles = {
+        comments = [ "italic" ];
+      };
+      transparentBackground = false;
+    };
+    vimAlias = false;
+    options = {
+      number = true;
+      shiftwidth = 2;
+      expandtab = true;
+      smartcase = true;
+      tabstop = 2;
+      smartindent = true;
+      cursorline = true;
+    };
+    plugins.lsp = {
+      enable = true;
+      servers = {
+        ccls.enable = true;
+      };
+    };
+    plugins.nvim-cmp = {
+      enable = true;
+      autoEnableSources = true;
+      sources = [
+        {name = "nvim_lsp";}
+        {name = "path";}
+        {name = "buffer";}
+      ];
+    };
+    plugins.telescope = {
+      enable = true;
+
+    };
+    plugins = {
+      airline.enable = true;
+    };
+    extraConfigVim = ''
+      set colorcolumn=80
+      let g:sonokai_style = 'default'
+      let g:sonokai_better_performance = 1
+      let g:airline_theme='sonokai'
+      let g:airline#extensions#tabline#enabled = 1
+      let g:airline_powerline_fonts = 1
+      let g:sonokai_transparent_background = 1
+      let g:sonokai_diagnostic_text_highlight = 1
+      let g:sonokai_spell_foreground = 'colored'
+      nnoremap <leader>ff <cmd>Telescope find_files<cr>
+      nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+      nnoremap <leader>fb <cmd>Telescope buffers<cr>
+      nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+    '';
+  };
 
   nixpkgs = {
     # You can add overlays here
@@ -62,7 +135,16 @@
 
   programs.vim = {
     enable = true;
-    plugins = with pkgs.vimPlugins; [ Vundle-vim clang_complete coc-clangd vim-airline vim-airline-themes sonokai ];
+    plugins = with pkgs.vimPlugins; [ 
+      Vundle-vim
+      clang_complete
+      coc-clangd
+      vim-airline
+      vim-airline-themes
+      sonokai
+      vim-nix
+      LanguageClient-neovim 
+    ];
     settings = {
       tabstop = 2;
       shiftwidth = 2;
