34d14cf34768c2af5283c5ff277877b11d9e857a
Alexander Romanov <alex.rom23@mail.ru>
2024-07-06 14:43:09 +0300

    feat:add zerotier

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 # Custom packages, that can be defined similarly to ones from nixpkgs
 # You can build them using 'nix build .#example'
 pkgs: {
-  # example = pkgs.callPackage ./example { };
+  zerotier-systemd-manager = pkgs.callPackage ./zerotier-systemd-manager.nix {};
 }
