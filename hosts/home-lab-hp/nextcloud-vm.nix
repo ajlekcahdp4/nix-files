c16538f5fe43ff78cd24094056a5ac9c9f4e7bef
Alexander Romanov <alex.rom23@mail.ru>
2024-07-21 09:10:51 +0300

    static example (NOT WORKING)

--- hosts/home-lab-hp/nextcloud-vm.nix
+++ hosts/home-lab-hp/nextcloud-vm.nix
@@ -1,14 +1,15 @@
 {
   pkgs,
   lib,
+  config,
   ...
 }: let
-  bridgeName = "bridge-to-nc-microvm-2";
+  bridgeName = "virbr0";
   vmMacAddr = "02:00:00:00:00:02";
-  vmTapId = "vm-nextcloud-2";
+  vmTapId = "vm-nextcloud";
 in {
   microvm.vms = {
-    nextcloud-microvm-2 = {
+    nextcloud-microvm = {
       autostart = true;
       config = {
         microvm = {
@@ -31,23 +32,27 @@ in {
             }
           ];
         };
-
         systemd.network = {
           enable = true;
-          networks."20-lan" = {
-            matchConfig.Name = "ether";
+          networks."${bridgeName}" = {
+            matchConfig.Name = bridgeName;
             networkConfig = {
               Address = ["192.168.90.3/24" "2001:db8::b/64"];
-              Gateway = "192.168.90.1";
-              DNS = ["192.168.90.1"];
+              Gateway = "192.168.90.2";
+              DNS = "192.168.90.2";
               IPv6AcceptRA = true;
-              DHCP = "no";
             };
           };
         };
-
-        networking.hostName = "nextcloud-microvm-2";
-        system.stateVersion = "24.05";
+        users.users.root.openssh.authorizedKeys.keys = [
+          (builtins.readFile ./microvm-key.pub)
+        ];
+        services.openssh = {
+          enable = true;
+          settings.PermitRootLogin ="yes";
+        };
+        networking.hostName = "nextcloud-microvm";
+        system.stateVersion = config.system.nixos.version;
       };
     };
   };
@@ -61,22 +66,32 @@ in {
       Kind = "bridge";
       Name = bridgeName;
     };
-
-    networks."10-lan" = {
+    networks."${bridgeName}" = {
       matchConfig.Name = bridgeName;
       networkConfig = {
         Address = ["192.168.90.2/24" "2001:db8::a/64"];
-        Gateway = "192.168.90.1";
-        DNS = "192.168.90.1";
+        Gateway = ["192.168.90.1"];
         IPv6AcceptRA = true;
       };
     };
-
-    networks."11-lan" = {
+    
+    networks."microvm-eth0" = {
       matchConfig.Name = "vm-*";
       networkConfig.Bridge = bridgeName;
     };
   };
+    
+  networking.nat = {
+    enable = true;
+    enableIPv6 = true;
+    internalInterfaces = [ bridgeName ];
+  };
+
   systemd.services."systemd-networkd".environment.SYSTEMD_LOG_LEVEL = "debug";
   networking.firewall.allowedUDPPorts = [67];
+  networking.extraHosts = ''
+    192.168.90.1 microvm-1
+    192.168.90.2 microvm-2
+    192.168.90.3 microvm-3
+  '';
 }
