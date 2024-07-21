129c2777cf07f6f4e42863cb1f1aa08d66258611
Alexander Romanov <alex.rom23@mail.ru>
2024-07-21 20:55:41 +0300

    working nextcloud in vm

--- hosts/home-lab-hp/nextcloud-vm.nix
+++ hosts/home-lab-hp/nextcloud-vm.nix
@@ -34,15 +34,6 @@ in {
         };
         systemd.network = {
           enable = true;
-          # networks."${bridgeName}" = {
-          #   matchConfig.Name = bridgeName;
-          #   networkConfig = {
-          #     Address = ["192.168.90.3/24" "2001:db8::b/64"];
-          #     Gateway = "192.168.90.2";
-          #     DNS = "192.168.90.2";
-          #     IPv6AcceptRA = true;
-          #   };
-          # };
         };
         users.users.root.openssh.authorizedKeys.keys = [
           (builtins.readFile ./microvm-key.pub)
@@ -50,8 +41,26 @@ in {
         services.openssh = {
           enable = true;
           settings.PermitRootLogin = "yes";
+          openFirewall = true;
         };
+
+        services.getty.autologinUser = "root";
+        services.nextcloud = {
+          enable = true;
+          hostName = "nextcloud.home";
+          home = "/var/lib/nextcloud-home";
+          package = pkgs.nextcloud29;
+          config = {
+            adminpassFile = "/etc/nextcloud-admin-pass";
+          };
+          settings = {
+            trusted_proxies = ["192.168.90.1"];
+            trusted_domains = ["localhost" "192.168.90.2"];
+          };
+        };
+        environment.etc."nextcloud-admin-pass".text = "11111111111";
         networking.hostName = "nextcloud-microvm";
+        networking.firewall.allowedTCPPorts = [22 80 443];
         system.stateVersion = config.system.nixos.version;
       };
     };
@@ -92,11 +101,6 @@ in {
           Prefix = "fd12:3456:789a::/64";
         }
       ];
-      # networkConfig = {
-      #   Address = ["192.168.90.2/24" "2001:db8::a/64"];
-      #   Gateway = ["192.168.90.1"];
-      #   IPv6AcceptRA = true;
-      # };
     };
 
     networks."microvm-eth0" = {
@@ -110,12 +114,18 @@ in {
     enableIPv6 = true;
     internalInterfaces = [bridgeName];
   };
-
+  services.nginx = {
+    enable = true;
+    virtualHosts."nextcloud.home" = {
+      locations."/" = {
+        proxyPass = "http://192.168.90.2:80";
+      };
+    };
+  };
   systemd.services."systemd-networkd".environment.SYSTEMD_LOG_LEVEL = "debug";
   networking.firewall.allowedUDPPorts = [67];
   networking.extraHosts = ''
-    192.168.90.1 microvm-bridge
-    192.168.90.2 microvm
+    192.168.90.2 nextcloud.home
   '';
   networking.useDHCP = lib.mkForce false;
 }
